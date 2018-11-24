5e0cd76ab37a8ec747b278ff7e86f3c18ec5d31d
fortym2 <30431538+fortym2@users.noreply.github.com>
2018-11-24 15:03:48 +0100

    Renaming function working but buggy A contentEditable div should become not editable when it loses focus.

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 /* eslint-disable no-console */
 const menuButtons = document.querySelectorAll(".menu a");
+const renameButtons = document.querySelectorAll(".menu-content a.rename");
+const removeButtons = document.querySelectorAll(".menu-content a.remove");
+const checklistNames = document.querySelectorAll(".header .name");
 
 function toggleChecklistMenu(event) {
-	console.log(event.path[4]);
 	const tasks = event.path[4].querySelector(".tasks");
 	const menuContent = event.path[4].querySelector(".menu-content");
 
@@ -17,4 +19,26 @@ function toggleChecklistMenu(event) {
 	tasks.classList.add("displayNone");
 }
 
+function toggleRename(event) {
+	const name = event.path[2].querySelector(".header .name");
+	name.contentEditable = true;
+	name.focus();
+}
+
+function keyDown(event) {
+	// finish editing on Enter key
+	if (event.which == 13) {
+		event.target.contentEditable = false;
+	}
+}
+
+function deleteChecklist(event) {
+	const checklist = event.path[2];
+	checklist.parentNode.removeChild(checklist);
+}
+
 menuButtons.forEach(btn => btn.addEventListener("click", toggleChecklistMenu));
+renameButtons.forEach(btn => btn.addEventListener("click", toggleRename));
+removeButtons.forEach(btn => btn.addEventListener("click", deleteChecklist));
+
+checklistNames.forEach(div => div.addEventListener("keydown", keyDown));

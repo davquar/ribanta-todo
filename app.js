f4cd27834fb34383fa1475c8c0cd892dfb64ed8e
fortym2 <30431538+fortym2@users.noreply.github.com>
2018-11-26 10:27:41 +0100

    Implemented event delegation, to make the checklist add function working without breaking anything.

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
 /* eslint-disable no-console */
-const menuButtons = document.querySelectorAll(".menu a");
+const newChecklistButton = document.querySelector("#btn-createList");
+const board = document.querySelector("#board");
+/* const menuButtons = document.querySelectorAll(".menu a");
 const renameButtons = document.querySelectorAll(".menu-content a.rename");
 const removeButtons = document.querySelectorAll(".menu-content a.remove");
 const checklistNames = document.querySelectorAll(".header .name");
+ */
+function newChecklist() {
+	board.innerHTML += `<div class="checklist">
+	<div class="header">
+		<div class="name smallTitle" onfocus="document.execCommand('selectAll', false, null);" onblur="notEditable()" >Nuova checklist</div>
+		<div class="menu"><a href="#"><img src="icons/rounded-menu.svg" alt="Menu"></a></div>
+	</div>
+	<div class="tasks"></div>
+	<div class="menu-content displayNone">
+		<a href="#" class="rename">Rinomina</a>
+		<a href="#" onclick="document.getElementById('id01').style.display='block'" class="w3-button" >Cambia colore</a>
+		<a href="#" class="remove">Elimina</a>
+	</div>
+</div>`;
+}
 
 function toggleChecklistMenu(event) {
-	const tasks = event.path[4].querySelector(".tasks");
-	const menuContent = event.path[4].querySelector(".menu-content");
+	const tasks = event.path[3].querySelector(".tasks");
+	const menuContent = event.path[3].querySelector(".menu-content");
 
 	if (menuContent.classList.contains("show")) {
 		menuContent.classList.remove("show");
@@ -25,13 +42,6 @@ function toggleRename(event) {
 	name.focus();
 }
 
-function keyDown(event) {
-	// finish editing on Enter key
-	if (event.which == 13) {
-		event.target.contentEditable = false;
-	}
-}
-
 function deleteChecklist(event) {
 	const checklist = event.path[2];
 	checklist.parentNode.removeChild(checklist);
@@ -47,7 +57,18 @@ function getColor(col){
 	document.getElementById("cl1").style.backgroundColor=col;
 }
 
-menuButtons.forEach(btn => btn.addEventListener("click", toggleChecklistMenu));
-renameButtons.forEach(btn => btn.addEventListener("click", toggleRename));
-removeButtons.forEach(btn => btn.addEventListener("click", deleteChecklist));
-checklistNames.forEach(div => div.addEventListener("keydown", keyDown));
+function handleBoardClicks(event) {
+	if (event.target.matches(".menu")) toggleChecklistMenu(event);
+	if (event.target.matches(".menu-content a.rename")) toggleRename(event);
+	if (event.target.matches(".menu-content a.remove")) deleteChecklist(event);
+}
+
+function handleBoardKeys(event) {
+	// finish editing on Enter key
+	if (event.target.matches(".header .name") && event.which == 13) {
+		event.target.contentEditable = false;
+	}
+}
+newChecklistButton.addEventListener("click", newChecklist);
+board.addEventListener("click", handleBoardClicks);
+board.addEventListener("keydown", handleBoardKeys);
